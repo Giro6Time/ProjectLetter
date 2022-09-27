9ee95bbedfb7f88d45bc62e532f2cc4f54f66174
Giro6Time <3139106326@qq.com>
2022-09-27 23:03:58 +0800

    2022/9/27 23:03

--- Assets/Scripts/Character/Hero.cs
+++ Assets/Scripts/Character/Hero.cs
@@ -1,17 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class Hero : Singleton<Hero>//是个单例
+public enum ConfidenceType
 {
+    timid = 0,
+    confident = 1
+}
+public class Hero : Singleton<Hero>
+{
+    
+    /// <summary>
+    /// 最大生命值
+    /// </summary>
     public float maxHP;
+    /// <summary>
+    /// 最大信任值
+    /// </summary>
     public float maxTrust;
     [SerializeField]    float hp;
     [SerializeField]    float trust;
-    [SerializeField]    float confident;
-    public int level;
+    [SerializeField]    ConfidenceType confident;
+    [SerializeField]    float atk;
+    /// 升级总共需要的经验值
+    [SerializeField]    float ExpInNeed;
+    /// 当前经验值
     float Experience;
+    
 
+    #region 外部访问的属性
     /// <summary>
     /// 角色生命值，小于等于角色最大生命值
     /// </summary>
@@ -41,10 +57,44 @@ public class Hero : Singleton<Hero>//是个单例
             UI_Trust.Instance.UpdateTrustValue();
         }
     }
-    private void Start()
+    /// <summary>
+    /// 当前自信等级
+    /// </summary>
+    public ConfidenceType Confident
     {
+        get => confident;
+    }
+    /// <summary>
+    /// 角色的自信等级经验值
+    /// </summary>
+    public float EXP
+    {
+        set
+        {
+            Experience = value;
+            if (Experience > ExpInNeed)
+            {
+                Experience -= ExpInNeed;
+                confident++;
+                UpdateConfident();
+            }
+        }
+    }
+    void UpdateConfident()
+    {
+        //TODO: 根据配置文件给对应confident等级设置升级所需经验和攻击力等数值
+    }
+    #endregion
+
+    #region 角色行为
+    //妹说我就不写
+#endregion
+
+    protected override void Init()//把Unity界面中预先配置的数值更新到游戏内
+    {
+        base.Init();
         Trust = trust;
-        HP = HP;
+        HP = hp;
     }
     private void Update()
     {
