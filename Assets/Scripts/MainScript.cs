ce054508c23c41fd804e856f818612645a8f9842
KUJIMML <1724430339@qq.com>
2022-10-04 19:14:22 +0800

    修改了房间以适配界面

--- Assets/Scripts/MainScript.cs
+++ Assets/Scripts/MainScript.cs
@@ -26,7 +26,8 @@ public class MainScript : MonoBehaviour
         room = 1;
         roomNo = 1;
         hero = GameObject.Find("Wizard").GetComponent<Hero>();
-        Layout(roomNo);  //生成初始房间
+        GetComponent<MapGeneration>().GenerateRoom(1, 1);  //生成初始房间
+        
     }
 
     void Update()
@@ -35,30 +36,6 @@ public class MainScript : MonoBehaviour
 
     }
 
-    ///布局函数，传入房间号，读取
-    ///点击出口时调用此函数
-    public void Layout(int RoomNo) 
-    {
-        InitRoom();  //移动旧房间的元素到对象池
-        ///下面根据表格布置新房间
-
-
-        Dialogue(RoomNo);  //
-    }
-
-    public void InitRoom()  ///清除旧房间
-    {
-        
-    }    
-
-    ///传入csv和行数，返回该行的stringp[]
-    public string[] ReadCsv (TextAsset text,int no)
-    {
-        string[] Data = { "" };
-        ///
-
-        return Data;
-    }
 
     ///信任度判定
     public bool TrustJudge()
