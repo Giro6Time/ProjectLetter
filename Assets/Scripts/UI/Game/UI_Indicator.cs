a1538cd7f8458019c82da58c05f41b6f1f97256d
WANG-Ruipeng <783132025@qq.com>
2022-10-21 15:20:44 +0800

    小地图脚本

---
+++ Assets/Scripts/UI/Game/UI_Indicator.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class UI_Indicator : MonoBehaviour
+{
+    List<string[]> IndicatorPosition;
+    RectTransform indicatorRectTransform;
+
+    private void Start()
+    {
+        string AssetPath;
+        string[] path = AssetDatabase.FindAssets("MapGeneration");
+        if (path.Length > 1) return;
+        AssetPath = AssetDatabase.GUIDToAssetPath(path[0]).Replace((@"/" + "MapGeneration" + ".cs"), "");
+        csvController.GetInstance().loadFile(AssetPath, "ToIndicatorPosition.csv");
+        IndicatorPosition = new List<string[]>(csvController.GetInstance().arrayData);
+
+        indicatorRectTransform = GetComponent<RectTransform>();
+    }
+
+    Vector3 SetIndicatorPosition(int RoomNo)
+    {
+        Vector3 indicatorPos = Vector3.zero;
+        if(IndicatorPosition[RoomNo][1]!=""&& IndicatorPosition[RoomNo][2] != "")
+        {
+            indicatorPos.x = float.Parse(IndicatorPosition[RoomNo][1]);
+            indicatorPos.y = float.Parse(IndicatorPosition[RoomNo][2]);
+            indicatorPos.z = 0;
+        }
+
+        indicatorRectTransform.SetPositionAndRotation(indicatorPos, Quaternion.Euler(Vector3.zero));
+        return indicatorPos;
+    }
+
+
+}
---
+++ Assets/Scripts/UI/Game/UI_Map.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Map : MonoBehaviour
+{
+    public void UpdateMapTo(int RoomNo)
+    {
+        
+    }
+}
